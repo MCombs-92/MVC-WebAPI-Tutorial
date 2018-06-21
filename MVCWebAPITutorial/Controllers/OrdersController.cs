d280a3944c5edb3d93cf2c55a936952c139bd2a2
Mitchell Combs <mitch.combs.92@gmail.com>
2018-06-21 16:29:09 -0400

    Initial Commit

---
+++ MVCWebAPITutorial/Controllers/OrdersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVCWebAPITutorial.Models;
+
+namespace MVCWebAPITutorial.Controllers {
+
+
+    public class OrdersController : ApiController {
+
+        private MVCWebAPIDBContext db = new MVCWebAPIDBContext();
+
+        [HttpGet]
+        [ActionName("List")]
+        public IEnumerable<Order> List() {
+            return db.Orders.ToList();
+        }
+
+        [HttpGet]
+        [ActionName("Get")]
+        public Order Get(int? id) {
+            if (id == null) {
+                return null;
+            }
+            return db.Orders.Find(id);
+
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public bool Create(Order order) {
+            if (order == null) {
+                return false;
+            }
+            if (!ModelState.IsValid) {
+                return false;
+            }
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return true;
+
+        }
+
+        [HttpPost]
+        [ActionName("Change")]
+        public bool Change(Order order) {
+            if (order == null) {
+                return false;
+            }
+            if (!ModelState.IsValid) {
+                return false;
+            }
+            var ord = db.Orders.Find(order.Id);
+            ord.Description = order.Description;
+            ord.Total = order.Total;
+            ord.CustomerId = order.CustomerId;
+            db.SaveChanges();
+            return true;
+        }
+
+        [HttpPost]
+        [ActionName("Remove")]
+        public bool Remove(Order order) {
+            if (order == null) {
+                return false;
+            }
+            if (!ModelState.IsValid) {
+                return false;
+            }
+            var ord = db.Orders.Find(order.Id);
+            db.Orders.Remove(ord);
+            db.SaveChanges();
+            return true;
+
+        }
+
+    }
+}
---
+++ MVCWebAPITutorial/Models/MVCWebAPIDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebAPITutorial.Models {
+
+
+    public class MVCWebAPIDBContext : DbContext {
+
+        public MVCWebAPIDBContext() : base() { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+
+}
